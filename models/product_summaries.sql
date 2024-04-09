78a8b7a1bed739970053ac6fd8d4e2a54d4eb78a
Yuki Shiga <y.shiga.91+yush1ga@gmail.com>
2024-04-10 01:31:34 +0900

    delete average_price column

--- models/product_summaries.sql
+++ models/product_summaries.sql
@@ -3,8 +3,7 @@ SELECT
     product_name,
     category_name,
     COUNT(order_id) AS total_orders,
-    SUM(order_amount * product_price) AS total_revenue,
-    AVG(product_price) AS average_price
+    SUM(order_amount * product_price) AS total_revenue
 FROM {{ ref('user_product_orders') }}
 GROUP BY product_id, product_name, category_name
 ORDER BY total_revenue DESC
